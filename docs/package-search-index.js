a5c6e68c78ac1089366b2366a9a0063a1e8fc18d
Lachlan Dowding <lachlan@permafrost.space>
2019-06-11 11:55:03 +1000

    Regenerate javadocs

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"permafrost.tundra.tn.delivery"},{"l":"permafrost.tundra.tn.document"},{"l":"permafrost.tundra.tn.lang"},{"l":"permafrost.tundra.tn.profile"}]
