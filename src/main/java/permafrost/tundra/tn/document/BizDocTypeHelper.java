0995106ca1a34ab6c9af4fe6c80bb2d92e091a03
Lachlan Dowding <lachlan@permafrost.space>
2021-03-26 06:03:22 +1000

    Fix BizDocTypeHelper.getNamespaceDeclarations(BizDocType) to infer the default namespace prefix to use for Integration Server by inspecting the root node for a prefix in the BizDocType's record blueprint

--- src/main/java/permafrost/tundra/tn/document/BizDocTypeHelper.java
+++ src/main/java/permafrost/tundra/tn/document/BizDocTypeHelper.java
@@ -24,23 +24,39 @@
 
 package permafrost.tundra.tn.document;
 
+import com.wm.app.b2b.server.ns.Namespace;
 import com.wm.app.tn.db.BizDocTypeStore;
-import com.wm.app.tn.doc.BizDocEnvelope;
 import com.wm.app.tn.doc.BizDocType;
 import com.wm.app.tn.doc.FFDocType;
 import com.wm.app.tn.doc.XMLDocType;
 import com.wm.data.IData;
 import com.wm.data.IDataCursor;
 import com.wm.data.IDataFactory;
+import com.wm.lang.ns.NSField;
 import com.wm.lang.ns.NSName;
+import com.wm.lang.ns.NSNode;
+import com.wm.lang.ns.NSRecord;
 import permafrost.tundra.data.IDataHelper;
-import permafrost.tundra.time.DateTimeHelper;
-import permafrost.tundra.tn.profile.ProfileHelper;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A collection of convenience methods for working with Trading Networks BizDocType objects.
  */
 public final class BizDocTypeHelper {
+    /**
+     * The default namespace prefix used by Trading Networks.
+     */
+    public static final String DEFAULT_NAMESPACE_PREFIX_TN = "prefix0";
+    /**
+     * The default namespace prefix used by Integration Server.
+     */
+    public static final String DEFAULT_NAMESPACE_PREFIX_IS = "ns";
+    /**
+     * Regular expression pattern used for matching against a namespace prefix in an XML tag name.
+     */
+    private static final Pattern NAMESPACE_PREFIX = Pattern.compile("((.+):)?(.+)");
+    
     /**
      * Disallow instantiation of this class.
      */
@@ -121,25 +137,30 @@ public final class BizDocTypeHelper {
             if (declarations != null) {
                 IDataCursor cursor = null;
                 try {
-                    boolean nsAlreadyExists = false;
-                    for (int i = 0; i < declarations.length; i++) {
-                        if (declarations[i] != null && declarations[i].length > 1) {
-                            String key = declarations[i][0];
-                            String value = declarations[i][1];
+                    String defaultNamespacePrefixIS = getDefaultNamespacePrefix(type);
+                    boolean defaultNamespacePrefixISAlreadyExists = false;
+                    for (String[] declaration : declarations) {
+                        if (declaration != null && declaration.length > 1) {
+                            String key = declaration[0];
+                            String value = declaration[1];
                             if (key != null && value != null) {
                                 if (namespace == null) namespace = IDataFactory.create();
                                 if (cursor == null) cursor = namespace.getCursor();
-                                if ("ns".equals(key)) nsAlreadyExists = true;
-                                if ("prefix0".equals(key) && !nsAlreadyExists) {
-                                    // By default Trading Networks uses the `prefix0` prefix to represent the default
-                                    // target namespace, whereas Integration Server uses the `ns` prefix.
-                                    // We will automatically add the `ns` prefix, unless its already been defined on the
-                                    // document type, with the same value as the `prefix0` prefix.
-                                    // Importantly, the `ns` prefix needs to precede the `prefix0` prefix in the
-                                    // resulting IData document's element insertion order, as the `WmPublic/pub.xml:*`
-                                    // services will use the first prefix in the document when parsing / serializing,
-                                    // and we want that to be the Integration Server default prefix `ns`.
-                                    IDataHelper.put(cursor, "ns", value);
+                                if (defaultNamespacePrefixIS.equals(key)) defaultNamespacePrefixISAlreadyExists = true;
+                                if (DEFAULT_NAMESPACE_PREFIX_TN.equals(key) && !defaultNamespacePrefixISAlreadyExists) {
+                                    // Trading Networks uses the `prefix0` prefix to represent the default target
+                                    // namespace. When importing an XML schema, Integration Server uses the `ns` prefix
+                                    // by default, however the prefix can be customised at import time to an arbitrary
+                                    // prefix. Therefore, we will try to determine the correct prefix to use by
+                                    // inspecting the first element on the record blueprint on the TN document type, and
+                                    // using the prefix specified there if applicable.
+                                    // Then we automatically add this Integration Server prefix, unless its already
+                                    // been defined on the document type, with the same value as the `prefix0` prefix.
+                                    // Importantly, the Integration Server prefix needs to precede the `prefix0` prefix
+                                    // in the resulting IData document's element insertion order, as the
+                                    // `WmPublic/pub.xml:*` services will use the first prefix in the document when
+                                    // parsing / serializing, and we want that to be the Integration Server prefix.
+                                    IDataHelper.put(cursor, defaultNamespacePrefixIS, value);
                                 }
                                 IDataHelper.put(cursor, key, value);
                             }
@@ -151,6 +172,64 @@ public final class BizDocTypeHelper {
             }
         }
         return namespace;
+    }
+
+    /**
+     * Returns the prefix to use for the default namespace by checking the root tag on the record blueprint on the
+     * given document type, if any.
+     *
+     * @param docType   The document type.
+     * @return          The prefix to use for the default namespace.
+     */
+    private static String getDefaultNamespacePrefix(BizDocType docType) {
+        return getDefaultNamespacePrefix(docType instanceof XMLDocType ? ((XMLDocType)docType).getRecordBlueprint() : null);
+    }
+
+    /**
+     * Returns the prefix to use for the default namespace by checking the root tag on the given node.
+     *
+     * @param nodeName  The document reference representing the XML structure.
+     * @return          The prefix to use for the default namespace.
+     */
+    private static String getDefaultNamespacePrefix(NSName nodeName) {
+        return getDefaultNamespacePrefix(nodeName == null ? null : Namespace.current().getNode(nodeName));
+    }
+
+    /**
+     * Returns the prefix to use for the default namespace by checking the root tag on the given node.
+     *
+     * @param node      The document reference representing the XML structure.
+     * @return          The prefix to use for the default namespace.
+     */
+    private static String getDefaultNamespacePrefix(NSNode node) {
+        return getDefaultNamespacePrefix(node instanceof NSRecord ? (NSRecord)node : null);
+    }
+
+    /**
+     * Returns the prefix to use for the default namespace by checking the root tag on the given record.
+     *
+     * @param record    The document reference representing the XML structure.
+     * @return          The prefix to use for the default namespace.
+     */
+    private static String getDefaultNamespacePrefix(NSRecord record) {
+        String prefix = DEFAULT_NAMESPACE_PREFIX_IS;
+        if (record != null) {
+            NSField[] fields = record.getFields();
+            if (fields != null && fields.length > 0) {
+                NSField field = fields[0]; // first field should be root tag
+                if (field != null) {
+                    String fieldName = field.getNCName();
+                    Matcher matcher = NAMESPACE_PREFIX.matcher(fieldName);
+                    if (matcher.matches()) {
+                        prefix = matcher.group(2);
+                        if (prefix == null) {
+                            prefix = ""; // use an empty string as prefix when no prefix is present on root tag
+                        }
+                    }
+                }
+            }
+        }
+        return prefix;
     }
 
     /**
